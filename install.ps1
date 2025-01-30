cd328e3eae8e7df8117c926b474af05e1f66aa74
just-hms <alessandroversari1@gmail.com>
2025-01-30 14:29:27 +0100

    add: mock installer

--- install.ps1
+++ install.ps1
@@ -51,22 +51,29 @@ foreach ($section in $iniConfig.Keys) {
     # Split the file extensions by comma and trim spaces
     $extensions = $files.Split(',') | ForEach-Object { $_.Trim() }
 
+    Write-Host "----"
+    Write-Host $name
+    Write-Host $files
+    Write-Host $titles
+    Write-Host "----"
+
+    continue
+
     # Find the script file
     $scriptPath = Join-Path $installPath $name
-    if (Test-Path $scriptPath) {
-        foreach ($ext in $extensions) {
-            # Register the script with the context menu for each file extension
-            $key = "HKCU:\Software\Classes\$ext\shell\Open with $title"
-            New-Item -Path $key -Force | Out-Null
-            New-ItemProperty -Path $key -Name "Icon" -Value "powershell.exe" -Force | Out-Null
-            New-Item -Path "$key\command" -Force | Out-Null
-            Set-ItemProperty -Path "$key\command" -Name "(default)" -Value "powershell.exe -ExecutionPolicy Bypass -File `"$scriptPath`"" | Out-Null
-
-            Write-Host "Added context menu entry for '$title' ($name) for extension '$ext'"
-        }
-    }
-    else {
+    if (-not (Test-Path $scriptPath)) {
         Write-Warning "Script '$name' not found at '$scriptPath'. Skipping context menu entry."
+        continue
+    }
+
+    foreach ($ext in $extensions) {
+        # Register the script with the context menu for each file extension
+        $key = "HKCU:\Software\Classes\$ext\shell\OpenWith$title"
+        New-Item -Path $key -Force | Out-Null
+        New-ItemProperty -Path $key -Name "Icon" -Value "powershell.exe" -Force | Out-Null
+        New-Item -Path "$key\command" -Force | Out-Null
+        Set-ItemProperty -Path "$key\command" -Name "(default)" -Value "powershell.exe -ExecutionPolicy Bypass -File `"$scriptPath`"" | Out-Null
+        Write-Host "Added context menu entry for '$title' ($name) for extension '$ext'"
     }
 }
 
